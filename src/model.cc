e534ac0868686f57ba1566ce324e0fd52869e95c
dinay-kingkiller <dinay.kingkiller@gmail.com>
2023-03-19 23:10:10 -0700

    Filled out ode function

--- src/model.cc
+++ src/model.cc
@@ -34,8 +34,10 @@
 
 #include "ros/ros.h"
 #include "quadcopter/FullState.h"
+#include "quadcopter/IMU.h"
 #include "quadcopter/Input.h"
 #include "quadcopter/State.h"
+
 
 namespace quadcopter {
 
@@ -43,27 +45,22 @@ namespace quadcopter {
 /// \brief
 class Model {
 private:
-  quadcopter::State current;
+  quadcopter::FullState last_state;
+  quadcopter::Input last_input;
+  ros::Time last_time;
   float mass;
   float arm_length;
   float k_gravity;
   float k_torque;
   float k_force; // b
-public:
-  Model(float Mass, float ArmLength, float GAccel, float kTorque, float kForce)
-    : mass(Mass),
-      arm_length(ArmLength),
-      k_gravity(GAccel),
-      k_torque(kTorque),
-      k_force(kForce) {}
-  /// \fn quadcopter::State Quadcopter::Model::ode(state, input)
+  /// \fn quadcopter::State quadcopter::Model::ode(state, input)
   /// \brief Given a state and input, returns the derivative of the state.
   quadcopter::FullState ode(quadcopter::FullState state, quadcopter::Input input) {
     quadcopter::FullState deriv;
     float input2 = input.front * input.front
-                 + input.right * input.right
-                 + input.back * input.back
-                 + input.left * input.left;
+      + input.right * input.right
+      + input.back * input.back
+      + input.left * input.left;
     deriv.x = state.diff_x;
     deriv.y = state.diff_y;
     deriv.z = state.diff_z;
@@ -73,29 +70,68 @@ public:
     deriv.diff_x = k_force / mass * sin(state.a) * input2;
     deriv.diff_y = - k_force / mass * cos(state.a) * sin(state.b) * input2;
     deriv.diff_z = k_force / mass * cos(state.a) * cos(state.b) * input2 - k_gravity;
+    // The conservation of angular momentum equation is more complicated
+    float m_inertia = mass * arm_length * arm_length;
+    float f0 = m_inertia * state.diff_b * state.diff_c * cos(state.c) * cos(state.a) / 2.0
+      + m_inertia * state.diff_a * state.diff_b * sin(state.a) * cos(state.c) / 2.0
+      - m_inertia * state.diff_a * state.diff_c * cos(state.c) * cos(state.c) / 2.0
+      + k_force * arm_length * (input.right*input.right - input.left*input.left);
+    float f1 = m_inertia * state.diff_a * state.diff_b * sin(state.a) * sin(state.c) / 2.0
+      - m_inertia * state.diff_b * state.diff_c * cos(state.a) * cos(state.c) / 2.0
+      + m_inertia * state.diff_a * state.diff_c * sin(state.c) / 2.0
+      + k_force * arm_length * (input.back*input.back - input.front*input.front);
+    float yaw_torque = input.front*input.front + input.back*input.back
+      - input.left*input.left - input.right*input.right;
+    float f2 = k_torque * yaw_torque - m_inertia * state.diff_a * state.diff_b * cos(state.a);
+    float mm00 = m_inertia * cos(state.a) * cos(state.c) / 2.0;
+    float mm01 = -m_inertia * cos(state.a) * sin(state.c) / 2.0;
+    float mm02 = m_inertia * sin(state.a);
+    float mm10 = m_inertia * sin(state.c) / 2.0;
+    float mm11 = m_inertia * cos(state.c) / 2.0;
+    if (mm01==0.0 or mm10==0.0) {throw std::runtime_error("Division by zero detected.");}
+    float inv_det = - 1.0 / (mm01 * mm10);
+    deriv.a = inv_det * (f0*mm11 - f1*mm01 - f2*mm02*mm11);
+    deriv.b = inv_det * (-f0*mm10 + f1*mm00 + f2*mm02*mm10);
+    deriv.c = inv_det * f2 * (mm00*mm11-mm01*mm10);
+  }
+public:
+  Model(float Mass, float ArmLength, float GAccel, float kTorque, float kForce,
+	quadcopter::FullState initial_state)
+    : last_state(initial_state),
+      mass(Mass),
+      arm_length(ArmLength),
+      k_gravity(GAccel),
+      k_torque(kTorque),
+      k_force(kForce) {
+    last_time = ros::Time::now();
+    last_input.front = 0.0;
+    last_input.right = 0.0;
+    last_input.back = 0.0;
+    last_input.left = 0.0;
+  }
+  void update() {
+    ros::Time next_time = ros::Time::now();
+    float dt = next_time.toSec() - last_time.toSec();
+    quadcopter::FullState deriv = ode(last_state, last_input);
+    last_state.x = deriv.x * dt;
+    last_state.y = deriv.y * dt;
+    last_state.z = deriv.z * dt;
+    last_state.a = deriv.a * dt;
+    last_state.b = deriv.b * dt;
+    last_state.c = deriv.c * dt;
+    last_state.diff_x = deriv.diff_x * dt;
+    last_state.diff_y = deriv.diff_y * dt;
+    last_state.diff_z = deriv.diff_z * dt;
+    last_state.diff_a = deriv.diff_a * dt;
+    last_state.diff_b = deriv.diff_b * dt;
+    last_state.diff_c = deriv.diff_c * dt;
+  }
+  quadcopter::Input add_motor_noise(quadcopter::Input) {
+  }
+  quadcopter::IMU add_sensor_noise(quadcopter::IMU) {
   }
 };
 }
-
-// [nx, bx] = r
-// <ny, by> = p
-// <[nz, bx], nx> = y
-
-
-// f=[(k*wB**2 + k*wF**2 + k*wL**2 + k*wR**2)*sin(theta),
-// -(k*wB**2 + k*wF**2 + k*wL**2 + k*wR**2)*sin(phi)*cos(theta),
-// -g*m + (k*wB**2 + k*wF**2 + k*wL**2 + k*wR**2)*cos(phi)*cos(theta),
-// -L**2*m*(-dphi*dpsi*sin(psi)*cos(theta) - dphi*dtheta*sin(theta)*cos(psi) + dpsi*dtheta*cos(psi))/2 - L*k*wL**2 + L*k*wR**2,
-// -L**2*m*(-dphi*dpsi*cos(psi)*cos(theta) + dphi*dtheta*sin(psi)*sin(theta) - dpsi*dtheta*sin(psi))/2 + L*k*wB**2 - L*k*wF**2,
-// -L**2*dphi*dtheta*m*cos(theta) + b*wB**2 + b*wF**2 - b*wL**2 - b*wR**2]
-// mm=[[m, 0, 0, 0, 0, 0],
-// [0, m, 0, 0, 0, 0],
-// [0, 0, m, 0, 0, 0],
-// [0, 0, 0, -L**2*m*(-dphi*dpsi*sin(psi)*cos(theta) - dphi*dtheta*sin(theta)*cos(psi) + dpsi*dtheta*cos(psi))/2 + L**2*m*(-dphi*dpsi*sin(psi)*cos(theta) - dphi*dtheta*sin(theta)*cos(psi) + dpsi*dtheta*cos(psi) + cos(psi)*cos(theta))/2,
-// -L**2*m*(-dphi*dpsi*cos(psi)*cos(theta) + dphi*dtheta*sin(psi)*sin(theta) - dpsi*dtheta*sin(psi))/2 + L**2*m*(-dphi*dpsi*cos(psi)*cos(theta) + dphi*dtheta*sin(psi)*sin(theta) - dpsi*dtheta*sin(psi) - sin(psi)*cos(theta))/2,
-// -L**2*dphi*dtheta*m*cos(theta) + L**2*m*(dphi*dtheta*cos(theta) + sin(theta))],
-// [0, 0, 0, -L**2*m*(-dphi*dpsi*sin(psi)*cos(theta) - dphi*dtheta*sin(theta)*cos(psi) + dpsi*dtheta*cos(psi))/2 + L**2*m*(-dphi*dpsi*sin(psi)*cos(theta) - dphi*dtheta*sin(theta)*cos(psi) + dpsi*dtheta*cos(psi) + sin(psi))/2, -L**2*m*(-dphi*dpsi*cos(psi)*cos(theta) + dphi*dtheta*sin(psi)*sin(theta) - dpsi*dtheta*sin(psi))/2 + L**2*m*(-dphi*dpsi*cos(psi)*cos(theta) + dphi*dtheta*sin(psi)*sin(theta) - dpsi*dtheta*sin(psi) + cos(psi))/2, 0],
-// [0, 0, 0, 0, 0, -L**2*dphi*dtheta*m*cos(theta) + L**2*m*(dphi*dtheta*cos(theta) + 1)]]
 
 int main(int argc, char **argv) {
   return 0;
