15c061b8b0c6398555111a70836608f7065f4e89
Dinay Kingkiller <dinay.kingkiller@gmail.com>
2023-04-20 07:08:06 -0700

    Update model.cpp
    fixed normalization bug

--- src/model.cpp
+++ src/model.cpp
@@ -75,7 +75,7 @@ void Model::zero()
   state_.q.x = 0.0;
   state_.q.y = 0.0;
   state_.q.z = 0.0;
-  state_.q.w = 0.0;
+  state_.q.w = 1.0;
   state_.w.x = 0.0;
   state_.w.y = 0.0;
   state_.w.z = 0.0;
@@ -167,6 +167,7 @@ void Model::update(const ros::TimerEvent& e)
   // Normalize quaternion
   float norm2 = state_.q.x*state_.q.x + state_.q.y*state_.q.y
     + state_.q.z*state_.q.z + state_.q.w*state_.q.w;
+  ROS_ASSERT_MSG(norm2 < 0.001, "Unable to normalize quaternion.  ||q||^2 = %d", norm2);
   float inv_norm = 1.0 / sqrt(norm2);
   state_.q.x *= inv_norm;
   state_.q.y *= inv_norm;
